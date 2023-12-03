dcfe61daf97a296f9d57a19df5e3440d0e6808c4
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2023-12-02 23:39:55 -0500

    switched my code to semantic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hellop");
+document.addEventListener('DOMContentLoaded', function () {
+  var weatherSearchForm = document.querySelector('#weather-search');
+  var cityInput = document.querySelector('#city');
+  var cityNameSpan = document.querySelector('#city-name');
+  var searchHistoryList = document.querySelector('#search-history');
+  var temperatureUnit = 'fahrenheit'; // Default unit
+});  
